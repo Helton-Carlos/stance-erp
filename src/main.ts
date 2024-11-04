7e79c54b6679d7c7073b80c41f0a77958a5b7295
Helton Silva <helton.silva@melhorenvio.com>
2024-11-04 20:28:42 -0300

    feat: add lib naive-ui

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import './style.css'
 import App from './App.vue'
 import router from './router/index'
 import { createPinia } from "pinia";
+import naive from "naive-ui";
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).use(naive).mount('#app')
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,5 +8,27 @@ const test = ref<string>('index');
     <h1>
       {{ test }}
     </h1>
+    <n-space>
+      <n-button>Default</n-button>
+      
+      <n-button type="tertiary">
+        Tertiary
+      </n-button>
+      <n-button type="primary">
+        Primary
+      </n-button>
+      <n-button type="info">
+        Info
+      </n-button>
+      <n-button type="success">
+        Success
+      </n-button>
+      <n-button type="warning">
+        Warning
+      </n-button>
+      <n-button type="error">
+        Error
+      </n-button>
+    </n-space>
   </div>
 </template>

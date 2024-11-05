9f801f7227d02db042084a9ce1b3ec96bea45678
Helton Silva <helton.silva@melhorenvio.com>
2024-11-05 20:32:25 -0300

    fix: change in scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
-import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler',
-        additionalData: `@use "@/assets/scss/global.scss";`,
+        additionalData: `@use "src/assets/scss/global.scss" as *;`,
       },
     },
   },

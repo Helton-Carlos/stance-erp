8dec3c8946920036e758a5c22c521367a4a4ea90
Helton Silva <helton.silva@melhorenvio.com>
2024-12-18 10:02:33 -0300

    feat: page stock

---
+++ src/pages/st-page-stock/ControlStock.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { loader } from '../../utils/loader.ts';
+
+const loading = ref(true);
+
+const startLoader = async () => {
+  loading.value = await loader(1400); 
+};
+
+startLoader()
+
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <n-card title="Gráfico anual" class="card mb-4">
+        <n-skeleton v-if="loading" :repeat="30" />
+        <STChart v-else />
+      </n-card>
+
+      <n-card title="Gráfico anual" class="card mb-4">
+        <n-skeleton v-if="loading" :repeat="30" />
+        <STChart v-else />
+      </n-card>
+    </div>
+  </div>
+</template>
---
+++ src/pages/st-page-stock/StockList.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { loader } from '../../utils/loader.ts';
+
+const loading = ref(true);
+
+const startLoader = async () => {
+  loading.value = await loader(1400); 
+};
+
+startLoader()
+
+const title = ref<string>('Lista de estoque');
+</script>
+
+<template>
+  <div>
+    <n-card :title="title" class="shadow">
+      <n-skeleton 
+        v-if="loading"  
+        :height="250" 
+        :sharp="false" 
+        animated
+        size="large"  
+      />
+
+      <STTable v-else />
+    </n-card>
+  </div>
+</template>
